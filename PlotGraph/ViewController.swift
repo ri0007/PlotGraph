3d981354ef003349f34ec05a2d0b052cfe08ac5d
inoueryuichi <r_i0007@yahoo.co.jp>
2016-01-31 23:20:54 +0900

    初回のコミット

---
+++ PlotGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlotGraph
+//
+//  Created by 井上 龍一 on 2016/01/31.
+//  Copyright © 2016年 Ryuichi Inoue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
